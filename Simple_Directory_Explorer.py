9677c987176ccf6cc1a753be7e7c19eb42210183
Elumer-dot <82724211+KrishnenduMarathe@users.noreply.github.com>
2021-09-19 09:56:12 +0530

    Update Simple_Directory_Explorer.py

--- Simple_Directory_Explorer.py
+++ Simple_Directory_Explorer.py
@@ -18,7 +18,7 @@
                                 -------
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General PublicLicense v3 as published by
+    it under the terms of the GNU General PublicLicense as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
